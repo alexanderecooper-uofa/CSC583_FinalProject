928fbd4e384d627d7ec68ed41c71fb2ae71b3108
Alexander Cooper <alexanderecooper@arizona.edu>
2024-04-23 21:48:58 +0000

    reranked results with ChatGPT

--- env.py
+++ env.py
@@ -8,6 +8,6 @@ class Env:
             # need to update this path everytime I login to Cyverse
             self.data_dir = "../data/iplant/home/alexanderecooper/analyses/CSC583_FinalProject"
 
-        self.index_path = f"./index"
+        self.index_path = f"{self.data_dir}/index"
 
 env = Env()
